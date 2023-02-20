e9c06c88414f19fed161ac0ec4f5098bbac1a27f
DhaneshGavimath <dhanesh3063@gmail.com>
2023-02-21 01:23:12 +0530

    main file update

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import config
-from model import train_classes
+# from model import train_classes
+# from data_preprocess import clean_data
+while True:
+    user = str(input("Bot: Welcome to simple chat. How can I help you? [Enter 'q' for quit]\nMe: "))     
+    if user.lower() == 'q':
+        print("Bot: See you again!")
+        break
+    print("Bot: At this moment chatbot is in developement. Will be available soon!")
+    break
+    #process the input and do text preprocessing and get the vector and predict the intent
+    
+    # cleaned_text = clean_data(user)
 
-print("Bot: Welcome to my world!")
+    
